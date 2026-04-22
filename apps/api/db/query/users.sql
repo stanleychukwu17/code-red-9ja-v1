47affc12e59173349b636779dc9624593a61fede
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-04-22 19:19:26 +0100

    feat(db): rename other_name to middle_name in CreateUser query

--- apps/api/db/query/users.sql
+++ apps/api/db/query/users.sql
@@ -1,7 +1,7 @@
 -- name: CreateUser :one
 INSERT INTO users (
   email, phone, username, password_hash, last_name,
-  first_name, other_name, gender, date_of_birth, current_country,
+  first_name, middle_name, gender, date_of_birth, current_country,
   current_state, current_city
 )
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
