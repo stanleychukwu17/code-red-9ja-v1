9535f8ec6af4ec94a9b56b549bb4e34b61ac1425
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-04-22 19:27:06 +0100

    made utils modular

--- apps/api/internal/utils/respond.go
+++ apps/api/internal/utils/respond.go
@@ -3,10 +3,22 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Utils struct {
+	db *pgxpool.Pool
+}
+
+func NewUtils(db *pgxpool.Pool) *Utils {
+	return &Utils{
+		db: db,
+	}
+}
+
 // RespondJSON writes a JSON response with the given status code and body.
-func RespondJSON(w http.ResponseWriter, status int, body any) {
+func (u *Utils) RespondJSON(w http.ResponseWriter, status int, body any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	_ = json.NewEncoder(w).Encode(body)
