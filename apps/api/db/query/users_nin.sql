fb6cd8c084af89908a03129e7abd4fdf6423a085
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-04-24 11:32:00 +0100

    deleted

--- apps/api/db/query/users_nin.sql
+++
@@ -1,9 +0,0 @@
--- name: CreateUserNIN :one
-INSERT INTO users_nin (user_id, nin)
-VALUES ($1, $2)
-RETURNING id;
-
--- name: GetUserNINByUserID :one
-SELECT id, nin FROM users_nin
-WHERE user_id = $1 LIMIT 1;
-
