c48953c7aa3559b4ff361b6949e81fcc70a5b711
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-04-26 08:00:20 +0100

    feat: helper contains alot of functions that are used in different test suites

--- apps/api/test/helper.go
+++ apps/api/test/helper.go
@@ -2,13 +2,45 @@ package test
 
 import (
 	"free9ja/api/internal/config"
+	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
+	"sync"
 )
 
-func BeforeEach(t *testing.T) error {
-	godotenv.Load("../.env")
-	config.Load()
-	return nil
+var (
+	test_cfg *config.Config
+	once     sync.Once
+)
+
+// setupTestEnvironmentVariables prepares environment for testing with testcontainers
+func setupTestEnvironmentVariables() {
+	os.Setenv("IS_TESTING", "true")
+}
+
+// cleanupTestEnvironmentVariables removes test environment variables
+func cleanupTestEnvironmentVariables() {
+	os.Unsetenv("IS_TESTING")
+}
+
+// BeforeAll runs once before all tests
+func BeforeAll(t *testing.T) (*config.Config, error) {
+	once.Do(func() {
+		setupTestEnvironmentVariables()
+		test_cfg = config.Load()
+	})
+
+	return test_cfg, nil
+}
+
+// BeforeEach prepares environment for each test with testcontainers
+func BeforeEach(t *testing.T) (*config.Config, error) {
+	cfg, _ := BeforeAll(t)
+	setupTestEnvironmentVariables()
+	return cfg, nil
+}
+
+func AfterEach(t *testing.T) (*testing.T, error) {
+	cleanupTestEnvironmentVariables()
+	return t, nil
 }
