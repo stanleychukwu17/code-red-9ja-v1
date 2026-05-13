0b9888d67a2761f30c4b56f181db603889cc53fc
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-13 08:51:39 +0100

    feat: exapanded the siteSlice to save the settings of the sideBar

--- apps/web/src/redux/slice/siteSlice.ts
+++ apps/web/src/redux/slice/siteSlice.ts
@@ -2,28 +2,39 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface SiteState {
-  title: string;
-  openSidebar: boolean;
+  // sideBarState: This is used to determine if the sidebar is collapsed or expanded
+  sideBarState: "" | "collapsed" | "expanded";
+
+  //currentSideBarWidth: This is used to store the current width of the sidebar
+  currentSideBarWidth: string;
+
+  //allowOutletToBeResponsive: This is used to allow the <Outlet /> component in the main __root.tsx
+  // to be responsive when the sidebar is collapsed or expanded, if false <Outlet /> will take full width
+  allowOutletToBeResponsive: boolean;
 }
 
 const initialState: SiteState = {
-  title: "Free9ja",
-  openSidebar: false,
+  sideBarState: "",
+  currentSideBarWidth: "16rem",
+  allowOutletToBeResponsive: true,
 };
 
 export const siteSlice = createSlice({
   name: "site",
   initialState,
   reducers: {
-    setSiteTitle: (state, action: PayloadAction<string>) => {
-      state.title = action.payload;
+    setSidebarState: (state, action: PayloadAction<SiteState["sideBarState"]>) => {
+      state.sideBarState = action.payload;
     },
-    setOpenSidebar: (state, action: PayloadAction<boolean>) => {
-      state.openSidebar = action.payload;
+    setCurrentSideBarWidth: (state, action: PayloadAction<string>) => {
+      state.currentSideBarWidth = action.payload;
+    },
+    setAllowOutletToBeResponsive: (state, action: PayloadAction<boolean>) => {
+      state.allowOutletToBeResponsive = action.payload;
     },
   },
 });
 
-export const { setSiteTitle, setOpenSidebar } = siteSlice.actions;
+export const { setSidebarState, setCurrentSideBarWidth, setAllowOutletToBeResponsive } = siteSlice.actions;
 
 export default siteSlice.reducer;
