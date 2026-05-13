d66cfb3dc193c2e7b10ce3c243f2ca41c52132bc
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-13 08:48:15 +0100

    used to load the user site settings

---
+++ apps/web/src/components/LoadSitePrefrence.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {
+  setCurrentSideBarWidth,
+  setSidebarState,
+  setAllowOutletToBeResponsive,
+} from "@/redux/slice/siteSlice";
+
+export default function LoadSitePrefrence() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Get the site settings from the local storage
+    const siteSettings = localStorage.getItem("site");
+
+    if (!siteSettings) return;
+    const parsed = JSON.parse(siteSettings);
+
+    // Dispatch the actions to update the Redux store with the values from the local storage
+    dispatch(setSidebarState(parsed.sideBarState));
+    dispatch(setCurrentSideBarWidth(parsed.currentSideBarWidth));
+    dispatch(setAllowOutletToBeResponsive(parsed.allowOutletToBeResponsive));
+  }, []);
+
+  return null;
+}
