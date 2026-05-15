927c6b03e963850fb9a18f0e76a677344ea3fbcd
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-15 17:57:03 +0100

    added the url map for frontend and backend

--- apps/web/src/lib/config.ts
+++ apps/web/src/lib/config.ts
@@ -1,2 +1,19 @@
 // lib/config.ts
 export const APP_NAME = import.meta.env.VITE_APP_NAME;
+const API_URLO = import.meta.env.VITE_API_URL
+
+export const APP_URL = {
+  "auth": {
+    "signup": "/auth/signup",
+    "login": "/auth/login",
+    "verifyOtp": "/auth/verify-otp",
+    "onboarding": "/auth/onboarding"
+  },
+  "dashboard": "/dashboard",
+}
+
+export const API_URL = {
+  "registration": `${API_URLO}/auth/register`,
+  "login": `${API_URLO}/auth/login`,
+  "getAllCountries": `${API_URLO}api/v1/countries`,
+}
