d71ee3cb0db54863f11af6b0b88a50f76cedb48c
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-16 19:43:27 -1200

    added more url to auth

--- apps/web/src/lib/config.ts
+++ apps/web/src/lib/config.ts
@@ -1,6 +1,6 @@
 // lib/config.ts
 export const APP_NAME = import.meta.env.VITE_APP_NAME;
-const API_URLO = import.meta.env.VITE_API_URL
+const API_BASE = import.meta.env.VITE_API_URL
 
 export const APP_URL = {
   "auth": {
@@ -12,8 +12,12 @@ export const APP_URL = {
   "dashboard": "/dashboard",
 }
 
+const api = `${API_BASE}/api/v1`
 export const API_URL = {
-  "registration": `${API_URLO}/auth/register`,
-  "login": `${API_URLO}/auth/login`,
-  "getAllCountries": `${API_URLO}api/v1/countries`,
+  "auth": {
+    "registerPhaseSignUp": `${api}/auth/register_phase_signup`,
+    "verifyOtp": `${api}/auth/verify_otp`,
+    "resendOtp": `${api}/auth/resend_otp`,
+  },
+  "getAllCountries": `${api}/countries`,
 }
