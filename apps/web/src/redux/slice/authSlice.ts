b5f01cba60ddda98025e99c3db6aa6d19bbfeb8d
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-18 11:41:36 +0100

    extended the auth slice and added a new reducer to update the authSlice state

--- apps/web/src/redux/slice/authSlice.ts
+++ apps/web/src/redux/slice/authSlice.ts
@@ -4,10 +4,15 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 export interface AuthProps {
   country: string;
   countryId?: number;
+  iso2?: string;
   phoneNumber: string;
   email?: string;
   password?: string;
   confirmPassword?: string;
+  id?: number;
+  fakeId?: number;
+  dateTimeOtpSent?: string;
+  otpVerified?: "yes" | "no";
 }
 
 export interface AuthState {
@@ -25,12 +30,19 @@ export const authSlice = createSlice({
     setOnboardingData: (state, action: PayloadAction<AuthProps>) => {
       state.onboardingData = action.payload;
     },
+    updateOnboardingData: (state, action: PayloadAction<Partial<AuthProps>>) => {
+      if (state.onboardingData) {
+        state.onboardingData = { ...state.onboardingData, ...action.payload };
+      } else {
+        state.onboardingData = action.payload as AuthProps;
+      }
+    },
     clearOnboardingData: (state) => {
       state.onboardingData = null;
     },
   },
 });
 
-export const { setOnboardingData, clearOnboardingData } = authSlice.actions;
+export const { setOnboardingData, updateOnboardingData, clearOnboardingData } = authSlice.actions;
 
 export default authSlice.reducer;
