ad31b4a10904aa8eb4281c6eda069f3ba30595dd
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-17 21:31:00 -1200

    added queries for fetching all cities and states

--- apps/api/db/query/countries.sql
+++ apps/api/db/query/countries.sql
@@ -13,3 +13,14 @@ WHERE id = $1 and state_id = $2 LIMIT 1;
 -- name: ListCountries :many
 SELECT id, name, iso2, phonecode FROM c_countries
 ORDER BY name ASC;
+
+-- name: GetStatesByCountryID :many
+SELECT id, name FROM c_states
+WHERE country_id = $1
+ORDER BY name ASC;
+
+-- name: GetCitiesByStateID :many
+SELECT id, name FROM c_cities
+WHERE state_id = $1
+ORDER BY name ASC;
+
