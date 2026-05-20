7e50aab78f9a1e2f56ee36dc79ecb761d381a336
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-20 12:55:01 +0100

    refactored: removed unwanted code

--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -2,27 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import siteReducer from "@/redux/slice/siteSlice";
 import authReducer from "@/redux/slice/authSlice";
 
-// const isBrowser = typeof window !== "undefined";
-// const savedSiteState = isBrowser ? localStorage.getItem("site") : null;
-// const preloadedSiteState = savedSiteState ? JSON.parse(savedSiteState) : undefined;
-
 export const store = configureStore({
   reducer: {
     site: siteReducer,
     auth: authReducer,
   },
-});
-
-
-// every time the site state changes, save it to localStorage,
-// the site carries the user's theme preference
-let timeout: ReturnType<typeof setTimeout>;
-store.subscribe(() => {
-  clearTimeout(timeout);
-
-  timeout = setTimeout(() => {
-    localStorage.setItem("site", JSON.stringify(store.getState().site));
-  }, 200);
 });
 
 export type RootState = ReturnType<typeof store.getState>;
