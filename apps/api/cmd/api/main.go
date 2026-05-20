830f527722104cc1cf11207218b6d6eae2c75bed
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-20 12:38:09 +0100

    added cfg to creating of a new route

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -67,7 +67,7 @@ func newApp(ctx context.Context, cfg *config.Config) *App {
 	}
 
 	// Initialize router
-	r := router.New(pool, rdb)
+	r := router.New(cfg, pool, rdb)
 	addr := fmt.Sprintf(":%s", cfg.Port)
 
 	return &App{
