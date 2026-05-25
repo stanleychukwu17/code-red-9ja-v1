199c4a2a537ddb24b1c2aada982017d52730f242
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-25 04:37:15 +0100

    changed table name to show that they are redis tables

--- apps/api/internal/db/redis_table.go
+++ apps/api/internal/db/redis_table.go
@@ -3,42 +3,45 @@ package db
 const (
 	//--START-- for registration
 	// SET: RedisRegisteredUsernames is the Redis set key used to store and check for existing usernames.
-	RedisRegisteredUsernames = "registered_usernames"
+	RedisRegisteredUsernames = "register:registered_usernames"
 
 	// SET: RedisRegisteredEmails is the Redis set key used to store and check for existing email addresses.
-	RedisRegisteredEmails = "registered_emails"
+	RedisRegisteredEmails = "register:registered_emails"
 
 	// SET: RedisRegisteredPhones is the Redis "set" key used to store and check for existing phone numbers.
-	RedisRegisteredPhones = "registered_phones"
+	RedisRegisteredPhones = "register:registered_phones"
 
 	// SET: RedisRegisteredNins is the Redis "set" key used to store and check for existing National Identification Numbers.
-	RedisRegisteredNins = "registered_nins"
+	RedisRegisteredNins = "register:registered_nins"
 	//--END--
 
 	//--START-- for login, refreshing jwt token, logout,
-	// STRING: jwtRefreshToken is the Redis "string" key used to store and retrieve a hashed jwt refresh token.
+	// STRING: "jwt:refresh_token:<hash>" is the Redis "string" key used to store and retrieve a hashed jwt refresh token.
 	// The key is a string, and the value is a JWT refresh token.
-	JwtRefreshToken = "jwt:refresh_token:"
+	RedisJwtRefreshToken = "jwt:refresh_token:"
 
-	// SET: login_sessions is the Redis "set" key used to store and check for existing login sessions.
-	LoginSessions = "login_sessions:"
+	// SET: "jwt:login_sessions:<sessionID>" is the Redis "set" key used to store and check for existing login sessions.
+	RedisLoginSessions = "jwt:login_sessions:"
 
-	// SET: user_login_sessions is the Redis "set" key used to store and check for existing user login sessions.
-	UserLoginSessions = "user_login_sessions:"
+	// SET: "jwt:user_login_sessions:<userFakeID>" is the Redis "set" key used to store and check for existing user login sessions.
+	RedisUserLoginSessions = "jwt:user_login_sessions:"
+
+	// STRING: user:info:<userFakeID> is the Redis "string" key used to store and retrieve user info.
+	RedisUserInfo = "user:info:"
 	//--END--
 
 	//--START-- for countries and states
-	// STRING: CountriesAll is the Redis "string" key used to store and retrieve all countries.
+	// STRING: "countries:all" is the Redis "string" key used to store and retrieve all countries.
 	// The key is a string, and the value is a JSON string of all countries.
 	CountriesAll = "countries:all"
 
-	// STRING: StatesByCountry is the Redis "string" key used to store and retrieve states of a country.
+	// STRING: "countries:states:<country_id>" is the Redis "string" key used to store and retrieve states of a country.
 	// The key is a string, and the value is a JSON string of all states of a country.
-	StatesByCountry = "states:country:"
+	StatesByCountry = "countries:states:"
 
-	// STRING: CitiesByState is the Redis "string" key used to store and retrieve cities of a state.
+	// STRING: "countries:cities:<state_id>" is the Redis "string" key used to store and retrieve cities of a state.
 	// The key is a string, and the value is a JSON string of all cities of a state.
-	CitiesByState = "cities:state:"
+	CitiesByState = "countries:cities:"
 	//--END--
 
 )
