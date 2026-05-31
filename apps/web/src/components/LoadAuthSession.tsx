1227c9479a6601ac7c120646ae88edcd0b585c81
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-05-31 08:56:02 +0100

    once we call on refreshing of new toke, we update userHydrated to true

--- apps/web/src/components/LoadAuthSession.tsx
+++ apps/web/src/components/LoadAuthSession.tsx
@@ -4,8 +4,8 @@ import { refreshUserToken } from "#/lib/server/auth/auth";
 import { updateAuthState } from "#/redux/slice/authSlice";
 
 // This component is used to restore the auth session on page refresh,
-// It will automatically refresh the access token every 14 minutes
-// and clear the auth data if the refresh token is invalid or missing
+// It will automatically refresh the accessToken and refreshToken every 14 minutes
+// and clear the auth data if the refreshToken is invalid or missing
 export default function LoadAuthSession() {
   const dispatch = useAppDispatch();
 
@@ -13,7 +13,6 @@ export default function LoadAuthSession() {
     const refreshSession = async () => {
       try {
         const response = await refreshUserToken();
-        dispatch(updateAuthState({ userHydrated: true }))
 
         if (response.status === "success") {
           const user = response.user;
@@ -22,14 +21,14 @@ export default function LoadAuthSession() {
       } catch (error) {
         dispatch(updateAuthState({ user: null }));
         console.error(`Error refreshing user token: `, error);
-      } 
+      }
     };
 
     // Initial refresh on mount
     refreshSession();
-    
+    dispatch(updateAuthState({ userHydrated: true }));
+
     // Periodically refresh the token every 14 minutes (since access token expires in 15 minutes)
-    // const interval = setInterval(() => refreshSession(), 14 * 60 * 1000);
     const interval = setInterval(() => refreshSession(), 14 * 60 * 1000); // refresh every 14 minutes until done
   
     return () => clearInterval(interval);
