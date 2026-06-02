f95895194c2cb212faa87dd522bc620ddbd385aa
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-02 19:02:53 +0100

    added new urls

--- apps/api/internal/utils/url_map.go
+++ apps/api/internal/utils/url_map.go
@@ -3,14 +3,15 @@ package utils
 // URLMap defines the structure for all API endpoints to avoid hardcoding strings.
 
 type AuthURLMap struct {
-	RegisterPhaseSignUp string
-	Register            string
-	CheckNin            string
-	CheckUsername       string
-	Login               string
-	Logout              string
-	ForgotPassword      string
-	Refresh             string
+	RegisterPhaseSignUp     string
+	Register                string
+	CheckNin                string
+	CheckUsername           string
+	Login                   string
+	Logout                  string
+	ForgotPassword          string
+	Refresh                 string
+	VerifySecurityQuestions string
 }
 
 type CountriesURLMap struct {
@@ -32,14 +33,15 @@ var ApiUrls = URLMap{
 	Health: "/health",
 	Root:   "/api/v1",
 	Auth: AuthURLMap{
-		RegisterPhaseSignUp: "/api/v1/auth/register_phase_signup",
-		CheckNin:            "/api/v1/auth/check_nin",
-		CheckUsername:       "/api/v1/auth/check_username",
-		Register:            "/api/v1/auth/register",
-		Login:               "/api/v1/auth/login",
-		Logout:              "/api/v1/auth/logout",
-		ForgotPassword:      "/api/v1/auth/forgot_password",
-		Refresh:             "/api/v1/auth/refresh",
+		RegisterPhaseSignUp:     "/api/v1/auth/register_phase_signup",
+		CheckNin:                "/api/v1/auth/check_nin",
+		CheckUsername:           "/api/v1/auth/check_username",
+		Register:                "/api/v1/auth/register",
+		Login:                   "/api/v1/auth/login",
+		Logout:                  "/api/v1/auth/logout",
+		ForgotPassword:          "/api/v1/auth/forgot_password",
+		Refresh:                 "/api/v1/auth/refresh",
+		VerifySecurityQuestions: "/api/v1/auth/verify_security_questions",
 	},
 	Countries: CountriesURLMap{
 		GetAll:    "/api/v1/countries",
