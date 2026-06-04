358845604899e5f17c5c5b1066ed353efffd2b48
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-04 19:33:25 +0100

    now loading the sitePrefrence before the page loads so i can preload the details and apply them before site loads

--- apps/web/src/components/OutletWrapper.tsx
+++ apps/web/src/components/OutletWrapper.tsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '@/redux/hooks';
-import { updateSiteState } from '@/redux/slice/siteSlice';
+import { updateSiteState, type SiteState } from '@/redux/slice/siteSlice';
 import { useIsMobile } from '@repo/ui/hooks/useMobile';
 import { useLocation } from '@tanstack/react-router';
 
 interface OutletWrapperProps {
   children: React.ReactNode;
+  sitePreference?: SiteState | null;
 }
 
 const listToHide = ['/auth/*', '/auth'];
 
-export function OutletWrapper({ children }: OutletWrapperProps) {
-  const location = useLocation();
-  const hideSideBar = listToHide.some(path => location.pathname.startsWith(path));
-  const { sideBarState, allowOutletToBeResponsive } = useAppSelector((state) => state.site);
-  const isMobile = useIsMobile();
+export function OutletWrapper({ children, sitePreference }: OutletWrapperProps) {
   const dispatch = useAppDispatch();
-  const [sidebarWidth, setSidebarWidth] = useState('16rem');
+  const reduxSiteState = useAppSelector((state) => state.site);
+  const location = useLocation();
+  const isMobile = useIsMobile();
+  const hideSideBar = listToHide.some(path => location.pathname.startsWith(path));
+
+  const isReduxSynced = reduxSiteState.sideBarState !== "";
+  const sideBarState = isReduxSynced ? reduxSiteState.sideBarState : (sitePreference?.sideBarState || "expanded");
+  const allowOutletToBeResponsive = isReduxSynced ? reduxSiteState.allowOutletToBeResponsive : (sitePreference?.allowOutletToBeResponsive ?? true);
+  const [sidebarWidth, setSidebarWidth] = useState(sitePreference?.currentSideBarWidth || "16rem");
 
   // Get sidebar width from DOM
   useEffect(() => {
@@ -27,7 +32,6 @@ export function OutletWrapper({ children }: OutletWrapperProps) {
         const expandedWidth = computedStyle.getPropertyValue('--sidebar-width');
         const collapsedWidth = computedStyle.getPropertyValue('--sidebar-width-icon');
         const width = sideBarState === 'expanded' ? expandedWidth : collapsedWidth;
-        // console.log("Sidebar width:", width
 
         const trimmedWidth = width.trim();
         setSidebarWidth(trimmedWidth);
