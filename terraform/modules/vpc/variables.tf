22f03ae7ecf81fcd026f2d18bcc005adceb0904d
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-08 06:36:56 +0100

    vpc modules

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,34 @@
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., staging, production)"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones in the region"
+  default     = ["af-south-1a", "af-south-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for public subnets (ALBs)"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for private subnets (ECS Tasks)"
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for database/cache subnets (RDS, Redis)"
+  default     = ["10.0.20.0/24", "10.0.21.0/24"]
+}
