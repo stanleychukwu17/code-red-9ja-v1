dd2f970cad18157ecc14a317f31a6d5dfc081d30
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-11 08:58:01 +0100

    minor: moved things around

--- terraform/modules/elasticache/variables.tf
+++ terraform/modules/elasticache/variables.tf
@@ -8,6 +8,16 @@ variable "environment" {
   description = "Environment name (e.g., staging, production)"
 }
 
+variable "node_type" {
+  type        = string
+  description = "The compute size of the cache node"
+}
+
+variable "num_cache_nodes" {
+  type        = number
+  description = "The number of cache nodes in the cluster"
+}
+
 variable "redis_subnet_group_name" {
   type        = string
   description = "The name of the ElastiCache subnet group"
@@ -16,15 +26,4 @@ variable "redis_subnet_group_name" {
 variable "security_group_id" {
   type        = string
   description = "The security group ID for the Redis cluster"
-}
-
-variable "node_type" {
-  type        = string
-  description = "The compute size of the cache node"
-  default     = "cache.t3.micro"
-}
-
-variable "num_cache_nodes" {
-  type        = number
-  description = "The number of cache nodes in the cluster"
 }
