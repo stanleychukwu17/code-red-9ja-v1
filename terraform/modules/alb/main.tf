05c0fc7ebb488a4c98a7aa349f7b9ace0713e8c2
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-13 19:30:47 +0100

    moved out a variable not needed

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -53,14 +53,7 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-variable "certificate_arn" {
-  type        = string
-  description = "The ARN of the ACM certificate for HTTPS listener (optional)"
-  default     = ""
-}
-
 resource "aws_lb_listener" "https" {
-  # count             = var.certificate_arn != "" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
