54626be1060e29ffbc94e40401765bb4f7836c4b
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-13 19:28:42 +0100

    production env

--- terraform/environments/production/outputs.tf
+++ terraform/environments/production/outputs.tf
@@ -8,6 +8,21 @@ output "alb_dns_name" {
   description = "The DNS name of the ALB"
 }
 
+output "acm_certificate_arn" {
+  value       = module.acm.certificate_arn
+  description = "The ARN of the validated ACM certificate"
+}
+
+output "frontend_url" {
+  value       = module.cloudflare.frontend_url
+  description = "The custom domain URL for the frontend"
+}
+
+output "backend_url" {
+  value       = module.cloudflare.backend_url
+  description = "The custom domain URL for the backend API"
+}
+
 output "ecr_repository_url" {
   value       = module.ecs.ecr_repository_url
   description = "The URL of the ECR repository"
@@ -23,17 +38,22 @@ output "ecs_service_name" {
   description = "The name of the ECS service"
 }
 
-output "frontend_url" {
-  value       = module.cloudflare.frontend_url
-  description = "The custom domain URL for the frontend"
-}
-
-output "backend_url" {
-  value       = module.cloudflare.backend_url
-  description = "The custom domain URL for the backend API"
-}
-
 output "github_actions_role_arn" {
   value       = module.oidc.role_arn
   description = "The ARN of the IAM role to use in GitHub Actions for deployments"
+}
+
+output "bastion_public_ip" {
+  value       = module.bastion.bastion_public_ip
+  description = "The public IP of the Bastion host"
+}
+
+output "rds_db_endpoint" {
+  value       = module.rds.db_endpoint
+  description = "The connection endpoint of the RDS instance"
+}
+
+output "redis_endpoint" {
+  value       = module.elasticache.redis_endpoint
+  description = "The endpoint of the Redis instance"
 }
