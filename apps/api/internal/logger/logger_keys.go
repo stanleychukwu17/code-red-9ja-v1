6ce5e346b99649fe020cd8836aa281902231403c
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-13 19:10:47 +0100

    added logger keys to make logging more DRY

---
+++ apps/api/internal/logger/logger_keys.go
@@ -0,0 +1,26 @@
+package logger
+
+// Components
+const (
+	ComponentAuthService      = "auth_service"
+	ComponentCountriesHandler = "countries_handler"
+	ComponentMessagingService = "messaging_service"
+	ComponentRouter           = "router"
+)
+
+// Events & Operations
+const (
+	EventUserLoginSuccess     = "user_login_success"
+	EventTokenRefreshSuccess  = "token_refresh_success"
+	EventUserLogoutSuccess    = "user_logout_success"
+	EventSendingOTP           = "sending_otp"
+	EventHTTPRequest          = "http_request"
+	EventRedisPipelineFailed  = "redis_pipeline_failed"
+	EventFetchCountriesFailed = "fetch_countries_failed"
+	EventInvalidCountryID     = "invalid_country_id"
+	EventFetchStatesFailed    = "fetch_states_failed"
+	EventInvalidStateID       = "invalid_state_id"
+	EventFetchCitiesFailed    = "fetch_cities_failed"
+)
+
+
