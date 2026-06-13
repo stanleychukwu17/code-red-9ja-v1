56f1da55594b1a583e925a9eace7da5f8b99148b
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-13 19:32:49 +0100

    removed the old output and replaced with the new fronttend_url domain

--- terraform/modules/cloudflare/outputs.tf
+++ terraform/modules/cloudflare/outputs.tf
@@ -1,17 +1,8 @@
-output "pages_project_name" {
-  value       = cloudflare_pages_project.frontend.name
-  description = "The name of the Cloudflare Pages project"
-}
-
-output "pages_subdomain" {
-  value       = cloudflare_pages_project.frontend.subdomain
-  description = "The pages.dev subdomain assigned to the project"
-}
-
 output "frontend_url" {
-  value       = "https://${cloudflare_pages_domain.frontend.name}"
-  description = "The custom URL of the frontend"
+  value       = length(cloudflare_workers_custom_domain.frontend) > 0 ? "https://${cloudflare_workers_custom_domain.frontend[0].hostname}" : null
+  description = "The custom domain URL for the frontend"
 }
+
 
 output "backend_url" {
   value       = "https://${var.backend_subdomain}.${var.domain_name}"
