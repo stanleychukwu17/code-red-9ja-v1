ec5d53bde2757e26c9743197846eb2bd0023a258
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-16 15:51:44 +0100

    fixed the error for, the health point was returning a 401 if the reuqest the not come with the AccessToken and refreshToken cookies

--- apps/api/internal/handler/handler.go
+++ apps/api/internal/handler/handler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 
 	"free9ja/api/internal/utils"
@@ -17,22 +16,20 @@ var utilsInstance = utils.NewUtils(nil)
 // @Success 200 {object} map[string]string
 // @Router /health [get]
 func Health(w http.ResponseWriter, r *http.Request) {
-	accessToken, err := r.Cookie("accessToken")
-	refreshToken, err := r.Cookie("refreshToken")
-	// fmt.Println("accessToken", accessToken)
-	// fmt.Println("refreshToken", refreshToken)
+	// accessToken, err := r.Cookie("accessToken")
+	// refreshToken, err := r.Cookie("refreshToken")
+	// // fmt.Println("accessToken", accessToken)
+	// // fmt.Println("refreshToken", refreshToken)
 
-	if err != nil {
-		// http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		utilsInstance.RespondError(w, http.StatusUnauthorized, "Unauthorized")
-		return
-	}
+	// if err != nil {
+	// 	// http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	// 	utilsInstance.RespondError(w, http.StatusUnauthorized, "Unauthorized")
+	// 	return
+	// }
 
-	accessTokenValue := accessToken.Value
-	refreshTokenValue := refreshToken.Value
-	_ = fmt.Sprintf("%s:%s", accessTokenValue, refreshTokenValue)
-
-	// utilsInstance.RespondSuccess(w, http.StatusOK, "Server running successfully", nil)
+	// accessTokenValue := accessToken.Value
+	// refreshTokenValue := refreshToken.Value
+	// _ = fmt.Sprintf("%s:%s", accessTokenValue, refreshTokenValue)
 	utilsInstance.RespondJSON(w, http.StatusOK, map[string]string{"status": "ok"})
 }
 
