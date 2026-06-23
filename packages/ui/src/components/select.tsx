f0b40315d185814d25fb2745828af94e0c285f00
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-06-23 18:00:37 +0100

    making sure the select color responds to different themes

--- packages/ui/src/components/select.tsx
+++ packages/ui/src/components/select.tsx
@@ -44,7 +44,7 @@ function SelectTrigger({
       data-slot="select-trigger"
       data-size={size}
       className={cn(`
-        flex w-fit items-center justify-between gap-1.5 rounded-[4px] py-6 pr-2 pl-2.5 text-sm whitespace-nowrap
+        flex bg-black/5 w-fit items-center justify-between gap-1.5 rounded-[4px] py-6 pr-2 pl-2.5 text-sm whitespace-nowrap
         transition-colors outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50
         disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20
         data-placeholder:text-muted-foreground data-[size=default]:h-8 data-[size=sm]:h-7 data-[size=sm]:rounded-[min(var(--radius-md),4px)]
