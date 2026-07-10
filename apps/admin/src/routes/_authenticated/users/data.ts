190b01bec150504c0788256691c768238f3625fe
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-07-10 11:37:45 +0100

    renamed

--- apps/admin/src/routes/_authenticated/users/data.ts
+++
@@ -1,7 +0,0 @@
-export type UsersTabId = "admin" | "app-users";
-
-export const USERS_TABS = [
-  { id: "admin", label: "Admin", href: "/users/admin" },
-  { id: "party-members", label: "Party members", href: "/users/party-members" },
-  { id: "users", label: "Users", href: "/users/users" },
-];
