1f7ecd6cc40539fb9fbc9ede04d2c4bc7cf14213
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-07-12 04:57:01 +0100

    using country reducer for countries

--- apps/partyadmin/src/redux/store.ts
+++ apps/partyadmin/src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import siteReducer, { sitePreferenceMiddleware } from "@/redux/slice/siteSlice";
 import authReducer from "@/redux/slice/authSlice";
 import electionReducer from "@/redux/slice/electionSlice";
+import countryReducer from "@/redux/slice/countrySlice";
 
 export const store = configureStore({
   reducer: {
     site: siteReducer,
     auth: authReducer,
     election: electionReducer,
+    country: countryReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sitePreferenceMiddleware),
