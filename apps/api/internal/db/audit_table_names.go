7eb9ff03cc87d0397edeef625b0451fa009bdc39
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-07-29 17:40:52 +0100

    added a new action for updating of user roles, then deleted redundant actions

--- apps/api/internal/db/audit_table_names.go
+++ apps/api/internal/db/audit_table_names.go
@@ -2,19 +2,17 @@ package db
 
 const (
 	// -- Modules --
-	ModuleAdmin = "admin" // Represents the administration module of the system
+	ModuleAdmin      = "admin"       // Represents the administration module of the system
+	ModulePartyAdmin = "party_admin" // Represents the party administration module of the system
 
 	// -- Actions --
 	ActionAssignPageVerification = "assign_page_verification" // Action when a verification badge is assigned to a user's page
 	ActionRemovePageVerification = "remove_page_verification" // Action when a verification badge is removed from a user's page
-	ActionCreateParty            = "create_party"             // Action for creating a new political party
-	ActionUpdateParty            = "update_party"             // Action for updating an existing political party's details
+	ActionUpdateUserRoles        = "update_user_roles"        // Action for updating a user's roles
 
 	// -- Actor Roles --
-	ActorRoleAdmin = "admin" // A standard administrative user
-	// ActorRoleSuperAdmin      = "super_admin"       // A super administrator with full system access
+	ActorRoleAdmin      = "admin"       // A standard administrative user
 	ActorRolePartyAdmin = "party_admin" // An administrator for a specific political party
-	// ActorRoleSuperPartyAdmin = "super_party_admin" // A top-level administrator for political parties
 
 	// -- Entity Types --
 	EntityTypeUser  = "user"  // Entity type representing a regular user
