3336569bb8b98a5d219de12178677efd8a3aa7fd
Chukwu Stanley <stanleychukwu17@yahoo.com>
2026-07-30 17:17:29 +0100

    now seeding 10k users and admins

--- apps/api/scripts/seeds/register_new_users.sh
+++ apps/api/scripts/seeds/register_new_users.sh
@@ -19,13 +19,29 @@ curl -X POST ${BASE_URL}/api/v1/seed/users \
   -H "Content-Type: application/json" \
   -d @"1.2-politicians.json"
 
-sleep 1 # wait for 2seconds
+sleep 1 # wait for 1second
 
 # seed celebrities
 echo "Seeding celebrities to ${BASE_URL}/api/v1/seed/users..."
 curl -X POST ${BASE_URL}/api/v1/seed/users \
   -H "Content-Type: application/json" \
   -d @"1.3-celebrities.json"
+
+sleep 1 # wait for 1 second
+
+# seed 10k users
+echo "Seeding 10k users to ${BASE_URL}/api/v1/seed/users..."
+curl -X POST ${BASE_URL}/api/v1/seed/users \
+  -H "Content-Type: application/json" \
+  -d @"1.4-10kusers.json"
+
+sleep 5 # wait for 5 second
+
+# seed admins
+echo "Seeding admins and party roles to ${BASE_URL}/api/v1/seed/admins..."
+curl -X POST ${BASE_URL}/api/v1/seed/admins \
+  -H "Content-Type: application/json" \
+  -d @"1.5-admins.json"
 
 echo ""
 echo "Done."
